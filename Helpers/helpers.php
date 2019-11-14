09c314f0d55ceff79b2f0535137970affc4414e7
Noviyanto Rahmadi <novay@enterwind.com>
2019-11-15 04:20:00 +0800

    Update helpers.php

--- Helpers/helpers.php
+++ Helpers/helpers.php
@@ -23,3 +23,22 @@ if(!function_exists('kelurahan')) {
         return new \Modules\Wilayah\Entities\Kelurahan;
     }
 }
+
+if(!function_exists('logo')) {
+    function logo($id) {
+        if(file_exists(public_path("logo/$id.png"))):
+            $ext = '.png';
+        endif;
+        if(file_exists(public_path("logo/$id.jpg"))):
+            $ext = '.jpg';
+        endif;
+        if(file_exists(public_path("logo/$id.jpeg"))):
+            $ext = '.jpeg';
+        endif;
+        if(file_exists(public_path("logo/$id.gif"))):
+            $ext = '.gif';
+        endif;
+
+        return asset('logo/' . $id . $ext);
+    }
+}
